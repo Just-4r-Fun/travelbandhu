cf9b1ca20ba4c50447fd62dc20f3b0719b9911fc
Kanishk Gupta <kanishk.gupta@cleartrip.com>
2023-12-13 19:26:28 +0530

    change homepage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import HomePageIllustration from "@/assets/images/HomePageIllustration.png";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Text } from "@chakra-ui/react";
+import SearchPlaceholder from "@/components/SearchPlaceholder";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Image src={HomePageIllustration} alt={"HomePage"} />
+      <div className="flex flex-col w-full h-screen">
+        <Image
+          src={HomePageIllustration}
+          alt={"HomePage"}
+          className="w-full max-h-[320px]"
+        />
+        <div className="h-full bg-white flex-1 flex items-center flex-col">
+          <Text fontSize={"xxx-large"} fontWeight={500} className="mt-4">
+            TRAVEL BANDHU
+          </Text>
+          <Text fontSize={"medium"} fontWeight={500} className="mb-4">
+            Turning Travel Dreams into Effortless Realities
+          </Text>
+          <SearchPlaceholder />
+        </div>
       </div>
     </>
   );
