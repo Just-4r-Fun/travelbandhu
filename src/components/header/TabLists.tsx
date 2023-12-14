5a24f079bcb7c8b86a006b4da3a1d9a718ced693
khushahal <khushahal.sharma@cleartrip.com>
2023-12-14 13:18:28 +0530

    add the nav tabs

--- src/components/header/TabLists.tsx
+++ src/components/header/TabLists.tsx
@@ -1,45 +1,21 @@
 import { Tabs, TabList, Tab } from "@chakra-ui/react";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { ROUTES } from "@/constants/common";
-import { useDispatch } from "react-redux";
-import { useAppDispatch } from "@/redux/hooks";
-import { setCurrentRoute } from "@/redux/manager/currentRoute";
 import { useRouter } from "next/router";
 
-function TabLists({ selectedRoute }: any) {
-  console.log(" selectedRoute ", selectedRoute);
-
-  const dispatch = useAppDispatch();
-  const [tabIndex, setTabIndex] = useState(selectedRoute.index);
-
+function TabLists() {
   const router = useRouter();
-  const currentRoute = router.asPath.split("/")[1];
 
-  console.log(" currentRoute ", currentRoute);
-  
-  const findSelectedIndex = ROUTES.findIndex((r)=> r === currentRoute);
-
-
-  console.log(" findSlected Index ", findSelectedIndex);
+  const findSelectedIndex = ROUTES.findIndex(({ route }) => {
+    return router.asPath === route;
+  });
 
   return (
     <>
-      <Tabs
-        isManual
-        variant="soft-rounded"
-        index={findSelectedIndex}
-        onChange={(index) => {
-          console.log(" onChagnge the nav tabs  ", index);
-
-          setTabIndex(index);
-          dispatch(
-            setCurrentRoute({ routeName: ROUTES[tabIndex], index: tabIndex })
-          );
-        }}
-      >
+      <Tabs isManual variant="soft-rounded" index={findSelectedIndex}>
         <TabList>
-          {ROUTES.map((route) => {
+          {ROUTES.map(({ route, name }) => {
             return (
               <Tab
                 key={`${route}_nav_tabs`}
@@ -47,7 +23,7 @@ function TabLists({ selectedRoute }: any) {
                 marginRight={"18px"}
                 _selected={{ color: "#223040", bg: "white" }}
               >
-                <Link href={`/${route}`}>{route}</Link>
+                <Link href={route}>{name}</Link>
               </Tab>
             );
           })}
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,9 +1,45 @@
+import Bandhu from "@/assets/svg/Bandhu";
+import Buddy from "@/assets/svg/Buddy";
+import Explore from "@/assets/svg/Explore";
+import Feed from "@/assets/svg/Feed";
+import Gallery from "@/assets/svg/Gallery";
+import Routes from "@/assets/svg/Routes";
+import Vlogs from "@/assets/svg/Vlogs";
+
 export const ROUTES = [
-  "Explore",
-  "Feed",
-  "Vlogs",
-  "Buddy",
-  "Routes",
-  "AI Gallery",
-  "Ask Bandhu",
+  {
+    name: "Explore",
+    icon: Explore,
+    route: "/explore",
+  },
+  {
+    name: "Feed",
+    icon: Feed,
+    route: "/feed",
+  },
+  {
+    name: "Vlogs",
+    icon: Vlogs,
+    route: "/vlogs",
+  },
+  {
+    name: "Buddy",
+    icon: Buddy,
+    route: "/buddy",
+  },
+  {
+    name: "Routes",
+    icon: Routes,
+    route: "/routes",
+  },
+  {
+    name: "AI Gallery",
+    icon: Gallery,
+    route: "/gallery",
+  },
+  {
+    name: "Ask Bandhu",
+    icon: Bandhu,
+    route: "/ask-bandhu",
+  },
 ];
